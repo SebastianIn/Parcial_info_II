26f0fc4264180df93c6b0accf9530afe1c6eadb8
sebastian <sebastian.insignares@udea.edu.co>
2020-05-13 23:49:35 -0500

    feat(estructura): se deja la estructura basica de base de datos al crearse todas sus funciones, aunque en blanco.

--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/BaseDeDatos.cpp
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/BaseDeDatos.cpp
@@ -2,5 +2,106 @@
 
 BaseDeDatos::BaseDeDatos()
 {
+    codificador = new Codificador();
+    archivos = new ManejoArchivos();
+}
+
+BaseDeDatos::~BaseDeDatos()
+{
+    delete codificador;
+    delete archivos;
+}
+
+bool BaseDeDatos::AutenticarUsuario(string nombre, string clave)
+{
+
+    return true;
+}
+
+bool BaseDeDatos::AutenticarAdmin(string clave)
+{
+
+    return true;
+}
+
+void BaseDeDatos::cambiarClave(string nombre, string nuevaClave)
+{
 
 }
+
+map<char, pair<int, bool> > BaseDeDatos::consultarEstadoSalaDeCine(int sala)
+{
+    map<char, pair<int, bool> > estadoActual;
+
+    return estadoActual;
+}
+
+void BaseDeDatos::cambiarEstadoSala(map<char, pair<int, bool> > nuevoEstado)
+{
+
+}
+
+list<array<string, 5> > BaseDeDatos::verCartelera()
+{
+    list<array<string, 5> >  cartelera;
+
+    return cartelera;
+}
+
+void BaseDeDatos::eliminarPelicula(string id)
+{
+
+}
+
+void BaseDeDatos::agregarPelicula(array<string, 5> pelicula)
+{
+
+}
+
+vector<pair<string, string> > BaseDeDatos::consultarVentas(string dia)
+{
+    vector<pair<string, string> > ventas;
+    ventas.push_back(pair<string,string> ("1","10000"));
+    ventas.push_back(pair<string,string> ("2","15000"));
+    ventas.push_back(pair<string,string> ("3","10000"));
+    ventas.push_back(pair<string,string> ("4","15000"));
+    //TODO
+    return ventas;
+}
+
+void BaseDeDatos::agregarVenta(string dia, string valor, string pelicula)
+{
+
+}
+
+map<string, pair<string, string> > BaseDeDatos::verPrecios()
+{
+    map<string,pair<string,string>> tablaPrecios;
+    pair<string,string> tipoPrecio;
+    tipoPrecio.first = "General";
+    tipoPrecio.second = "10000";
+    tablaPrecios.insert(pair<string,pair<string,string>>("1",tipoPrecio));
+    tipoPrecio.first = "VIP";
+    tipoPrecio.second = "15000";
+    tablaPrecios.insert(pair<string,pair<string,string>>("2",tipoPrecio));
+    //TODO
+    return tablaPrecios;
+}
+
+void BaseDeDatos::agregarTipoPrecio(string idTipo, string nombre, string valor)
+{
+
+}
+
+void BaseDeDatos::eliminarTipoPrecio(string idTipo)
+{
+
+}
+
+void BaseDeDatos::modificarPrecio(string idTipo, string valor)
+{
+
+}
+
+
+
--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/BaseDeDatos.h
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/BaseDeDatos.h
@@ -1,11 +1,84 @@
 #ifndef BASEDEDATOS_H
 #define BASEDEDATOS_H
 
+#include "Codificador.h"
+#include "ManejoArchivos.h"
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+#include <array>
+
+using namespace std;
 
 class BaseDeDatos
 {
 public:
     BaseDeDatos();
+    ~BaseDeDatos();
+    /**************************************************************
+     *
+     *          Metodos para la manipulacion de Usuarios
+     *
+     *************************************************************/
+
+    bool AutenticarUsuario(string nombre, string clave);
+    bool AutenticarAdmin(string clave);
+    void cambiarClave(string nombre, string nuevaClave);
+
+    /**************************************************************
+     *
+     *          Metodos para la manipulacion de salas
+     *
+     *************************************************************/
+
+    map<char,pair<int,bool>> consultarEstadoSalaDeCine(int sala);
+    void cambiarEstadoSala(map<char,pair<int,bool>> nuevoEstado);
+
+    /**************************************************************
+     *
+     *          Metodos para el manejo de la cartelera.
+     *
+     *************************************************************/
+    /**
+     * @brief verCartelera metodo para solicitar la cartelera
+     * @return lista con array<string , 5> que contiene los datos
+     * id,nombre,genero,duracion,sala/hora,
+     */
+    list<array<string , 5>> verCartelera();
+    void eliminarPelicula(string id);
+    void agregarPelicula(array<string , 5> pelicula);
+
+    /**************************************************************
+     *
+     *          Metodos para el manejo de la cartelera.
+     *
+     *************************************************************/
+    /**
+     * @brief consultarVentas consulta la tabla de ventas del dia
+     * @param dia - dia a consultar con dormato dd-MM-YYYY
+     * @return mapa con <idPelicula, valor>
+     */
+    vector<pair<string,string>> consultarVentas(string dia);
+    void agregarVenta(string dia, string valor, string pelicula);
+
+    /**************************************************************
+     *
+     *          Metodos para Manejo de precios.
+     *
+     *************************************************************/
+    /**
+      * @brief verPrecios consulta la tabla de precios
+      * @return mapa con <idTipo,pair<nombre,valor>>
+      */
+     map<string,pair<string,string>> verPrecios();
+     void agregarTipoPrecio(string idTipo,string nombre, string valor);
+     void eliminarTipoPrecio(string idTipo);
+     void modificarPrecio(string idTipo, string valor);
+
+private:
+    Codificador* codificador;
+    ManejoArchivos* archivos;
 };
 
 #endif // BASEDEDATOS_H

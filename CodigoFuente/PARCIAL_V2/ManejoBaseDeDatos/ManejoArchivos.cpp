67a308b09e38328258b7d91df15569d7fd499792
sebastian <sebastian.insignares@udea.edu.co>
2020-05-13 22:40:20 -0500

    feat(ManejoDeArchivos): clase que se encarga de escribir y leer de archivos.

--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.cpp
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.cpp
@@ -4,3 +4,23 @@ ManejoArchivos::ManejoArchivos()
 {
 
 }
+
+void ManejoArchivos::leerArchivo(string nombreDeArchivo, string *contenido)
+{
+    ifstream archivo(nombreDeArchivo);
+    string textoLinea,texto = "";
+    while (!archivo.eof()) {
+        getline(archivo,textoLinea);
+        texto += textoLinea;
+    }
+    *contenido = texto;
+    archivo.close();
+}
+
+void ManejoArchivos::escribirArchivo(string nombreDeArchivo, string contenido)
+{
+    ofstream archivo(nombreDeArchivo);
+    archivo<<contenido;
+    archivo.close();
+}
+
--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.h
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.h
@@ -1,11 +1,17 @@
 #ifndef MANEJOARCHIVOS_H
 #define MANEJOARCHIVOS_H
 
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 class ManejoArchivos
 {
 public:
     ManejoArchivos();
+    void leerArchivo(string nombreDeArchivo, string *contenido);
+    void escribirArchivo(string nombreDeArchivo, string contenido);
 };
 
 #endif // MANEJOARCHIVOS_H

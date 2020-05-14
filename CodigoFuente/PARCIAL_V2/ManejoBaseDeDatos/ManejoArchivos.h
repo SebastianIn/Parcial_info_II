2ff25b699307ad54d3786e724705a39100a439a4
sebastian <sebastian.insignares@udea.edu.co>
2020-05-13 23:48:40 -0500

    feat(comentarios): se agregan comentarios en ManejoArchivos

--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.h
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/ManejoArchivos.h
@@ -10,7 +10,17 @@ class ManejoArchivos
 {
 public:
     ManejoArchivos();
+    /**
+     * @brief leerArchivo - Metodo para leer el contenido de un archivo
+     * @param nombreDeArchivo - Nombre del archivo a leer.
+     * @param contenido - apuntador donde se colocara el contnido leido
+     */
     void leerArchivo(string nombreDeArchivo, string *contenido);
+    /**
+     * @brief escribirArchivo - Metodo para escribir el contenido de un archivo
+     * @param nombreDeArchivo - Nombre del archivo a escribir.
+     * @param contenido - contenido a escribir en el archivo.
+     */
     void escribirArchivo(string nombreDeArchivo, string contenido);
 };
 

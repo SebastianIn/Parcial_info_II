d0df5401fbc777352f57cb9bcaa63ee99c512458
sebastian <sebastian.insignares@udea.edu.co>
2020-05-13 22:33:07 -0500

    feat(Codificador): Se completo la clase Codificador

--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/Codificador.cpp
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/Codificador.cpp
@@ -1,6 +1,114 @@
 #include "Codificador.h"
 
-Codificador::Codificador()
+Codificador::Codificador(unsigned long semillaP)
 {
+    semilla = semillaP;
+}
 
+string Codificador::codificar(string textoOriginal)
+{
+    string textoBinario = string2bin(textoOriginal);
+    string codificado = "";
+    string bloque = "";
+    string bloqueCodificado = "";
+    unsigned long posicion = 0;
+    unsigned long longitud = textoBinario.length();
+    unsigned long numeroDeBloques = longitud / semilla;
+    for(unsigned int i = 0; i < numeroDeBloques; i++){
+        bloque = textoBinario.substr(posicion,semilla);
+        posicion += semilla;
+        bloqueCodificado = correrBloque(bloque);
+        codificado.append(bloqueCodificado);
+    }
+    if(longitud % semilla != 0){
+        unsigned long faltante = longitud % semilla;
+        bloque = textoBinario.substr(posicion,faltante);
+        bloqueCodificado = correrBloque(bloque);
+        codificado.append(bloqueCodificado);
+    }
+    return bin2String(codificado);
+}
+
+string Codificador::decodificar(string textoCodificado)
+{
+    string textoBinario = string2bin(textoCodificado);
+    string decodificado = "";
+    string bloque = "";
+    string bloqueDecodificado = "";
+    unsigned long posicion = 0;
+    unsigned long longitud = textoBinario.length();
+    unsigned long numeroDeBloques = longitud / semilla;
+    for(unsigned int i = 0; i < numeroDeBloques; i++){
+        bloque = textoBinario.substr(posicion,semilla);
+        posicion += semilla;
+        bloqueDecodificado = devolverBloque(bloque);
+        decodificado.append(bloqueDecodificado);
+    }
+    if(longitud % semilla != 0){
+        unsigned long faltante = longitud % semilla;
+        bloque = textoBinario.substr(posicion,faltante);
+        bloqueDecodificado = devolverBloque(bloque);
+        decodificado.append(bloqueDecodificado);
+    }
+    return bin2String(decodificado);
+}
+
+string Codificador::string2bin(string texto)
+{
+    string binario = "";
+    unsigned int letra;
+    for (unsigned int i=0; i<texto.length(); i++) {
+        string binLetra[8];
+        letra = static_cast<unsigned int>(texto[i]);
+        for (int j=7;j>=0;j--) {
+            binLetra[j]=static_cast<char>(letra%2+48);
+            letra/=2;
+        }
+        for(int j=0; j<8; j++){
+            binario += binLetra[j];
+        }
+    }
+    return binario;
+}
+
+string Codificador::bin2String(string texto)
+{
+    string conversion = "";
+    unsigned long longitud = texto.size();
+    unsigned long cantidadDeCaracteres = longitud / 8;
+    unsigned long index=0;
+    unsigned long numero=0;
+    for(unsigned long i=0; i < cantidadDeCaracteres; i++){
+        numero=0;
+        index=8*i;
+        for(unsigned long j=0;j<8;j++){
+            if(texto[j+index] == '1'){
+               numero += static_cast<unsigned long>(pow(2,(7-j)));
+            }
+        }
+        conversion.push_back(static_cast<char>(numero));//[i] = static_cast<char>(numero);
+    }
+    return conversion;
+}
+
+string Codificador::correrBloque(string bloque)
+{
+    char primero = bloque.back();
+    unsigned long longituf = bloque.length();
+    for(unsigned long i = longituf - 1; i > 0; i--){
+         bloque[i] = bloque[i-1];
+    }
+    bloque[0] = primero;
+    return bloque;
+}
+
+string Codificador::devolverBloque(string bloque)
+{
+    char ultimo = bloque.front();
+    unsigned long ultimoPos = bloque.length() - 1;
+    for(unsigned long i = 0; i < ultimoPos; i++){
+         bloque[i] = bloque[i+1];
+    }
+    bloque[ultimoPos] = ultimo;
+    return bloque;
 }
--- CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/Codificador.h
+++ CodigoFuente/PARCIAL_V2/ManejoBaseDeDatos/Codificador.h
@@ -1,11 +1,86 @@
 #ifndef CODIFICADOR_H
 #define CODIFICADOR_H
 
+#include <string>
+#include <math.h>
 
+using namespace std;
+
+/**
+ * @brief The Codificador class
+ * Esta clase se encarga de codificar y decodificar la
+ * informacion de la base de datos.
+ * @author: Sebastian Insignares
+ */
 class Codificador
 {
 public:
-    Codificador();
+    /**
+     * @brief Codificador Constructor de la clase
+     * @param semillaP - parametro de codificacion.
+     */
+    Codificador( unsigned long semillaP = 4);
+
+    /**
+     * @brief codificar    - Codifica la informacion a guardar en la base de datos.
+     *
+     * @param textoBinario              - El texto en binario a codificar.
+     * @param semilla                   - La semilla de codificacions.
+     * @return el texto codificado.
+     */
+    string codificar(string textoOriginal);
+
+    /**
+     * @brief decodificar  - Decodifica la informacion leida de la base de datos.
+     *
+     * @param textoCodificado           - El texto en binario codificado.
+     * @param semilla                   - La semilla de codificacions.
+     * @return el texto decodificado.
+     */
+    string decodificar(string textoCodificado);
+
+private:
+    /**
+     * @brief semilla - parametro de codificacion.
+     */
+    unsigned long semilla;
+
+    /**
+     * @brief string2bin  - Este Metodo combierte una cadena de caracters, en una representacion
+     * de caracteres binarios. ejemplo: el caracter 'A' se combierte en "01000001".
+     *
+     * @param texto     - Es la cadena de caracteres que se quiere convertir.
+     * @return la cadena de caracteres convertida.
+     */
+    string string2bin(string texto);
+
+    /**
+     * @brief bin2String  - Este Metodo combierte una representacion de caracteres binarios
+     * en una cadena de caracters. ejemplo: la cadena "01000001" se combierte en el caracter 'A'.
+     *
+     * @param texto     - string donde se almacena la cadena de caracteres binarios.
+     * @return la cadena de caracteres convertida.
+     */
+    string bin2String(string texto);
+
+    /**
+     * @brief correrBloque  - Corre los caracteres del bloque un caracter a la derecha, de tal manera que el ultimo
+     * pasa a ser el primero.
+     *
+     * @param bloque        - bloque al que se le aplica el corrimiento.
+     * @return bloque con el corrimiento aplicado.
+     */
+    string correrBloque(string bloque);
+
+    /**
+     * @brief devolverBloque    - Corre los caracteres del bloque un caracter a la izquierda, de tal manera que el primero
+     * pasa a ser el ultimo.
+     *
+     * @param bloque            - bloque al que se le aplica el corrimiento.
+     * @return bloque con el corrimiento contrario aplicado.
+     */
+    string devolverBloque(string bloque);
+
 };
 
 #endif // CODIFICADOR_H
